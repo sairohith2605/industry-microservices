b9c3a6051fcc0cdb513ab309eefb75aa4b60ef66
Sai Rohith Reddy <sairohith2605@outlook.com>
2024-12-04 10:00:39 +0530

    Bootstrap an inventory project

---
+++ inventory/src/test/java/com/industry/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.industry.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
